692ffedd10ae9a7c2f6900ae6adc2a0c6cfda48e
Andrew Ng <ajng@princeton.edu>
2017-03-28 04:35:21 -0400

    ssl?

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template
+from flask_sslify import SSLify
+
 app = Flask(__name__)
+if 'DYNO' in os.environ: # only trigger SSLify if the app is running on Heroku
+    sslify = SSLify(app)
 
 @app.route('/')
 @app.route('/index')
