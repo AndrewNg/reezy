adc56ad8fb87bce486881d8f308b1f84147407dc
Andrew Ng <ajng@princeton.edu>
2017-04-02 04:01:09 -0400

    eh screw flash

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-from flask import Flask, render_template, request, flash, redirect
+from flask import Flask, render_template, request, redirect
 from flask_sslify import SSLify
 from werkzeug.utils import secure_filename
 import os, json
 
 app = Flask(__name__)
 app.debug = True # for autoreload
-app.secret_key = 'not very secret' # not actually important, just for flash to work
 if 'DYNO' in os.environ: # only trigger SSLify if the app is running on Heroku
   sslify = SSLify(app)
 
@@ -27,13 +26,11 @@ def status():
 def process():
   # check if the post request has the file part
   if 'file' not in request.files:
-      flash('No file part')
-      return json.dumps({'data':'no file dumbass'});
+    return json.dumps({'data':'no file dumbass'});
   file = request.files['file']
   # if user does not select file, browser also
   # submit a empty part without filename
   if file.filename == '':
-    flash('No selected file')
     return json.dumps({'data':'no file name dumbass'});
   if file and allowed_file(file.filename):
     filename = secure_filename(file.filename)
