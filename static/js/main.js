43e557a3fbd3c43c367b34fd3c6e118ed28dc4c8
samhitak <samhita.karnati@gmail.com>
2017-04-15 19:12:27 -0400

    fixing master

---
+++ static/js/main.js
@@ -0,0 +1,21 @@
+$('.button').click(function(e) {
+  e.preventDefault();
+  var form_data = new FormData($('#upload-file')[0]);
+  var progressBar = $('.progress');
+  var messageBox = $('.messages')
+  progressBar.removeClass('hide');
+  $.ajax({
+    type: 'POST',
+    url: '/process',
+    data: form_data,
+    contentType: false,
+    processData: false,
+    success: function(result) {
+      console.log(JSON.parse(result).data);
+      messageBox.html('<a href="' + JSON.parse(result).unique_url + '" download>Download the file!</a>')
+    },
+    error: function(result) {
+      console.log('error');
+    }
+    });
+});
