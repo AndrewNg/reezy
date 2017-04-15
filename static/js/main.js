e5150950bd4e04502eb8bf2030065b53618cd184
samhitak <samhita.karnati@gmail.com>
2017-04-15 19:30:18 -0400

    progress bar in js

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,9 @@
 $('.summary').click(function(e) {
   e.preventDefault();
   var form_data = new FormData($('#upload-file')[0]);
+  var progressBar = $('.progress');
+  var messageBox = $('.messages')
+  progressBar.removeClass('hide');
   $.ajax({
     type: 'POST',
     url: '/process',
@@ -8,7 +11,8 @@ $('.summary').click(function(e) {
     contentType: false,
     processData: false,
     success: function(result) {
-      console.log(JSON.parse(result).data + JSON.parse(result).unique_url);
+      console.log(JSON.parse(result).data);
+      messageBox.html('<a href="' + JSON.parse(result).unique_url + '" download>Download the file!</a>')
     },
     error: function(result) {
       console.log('error');
