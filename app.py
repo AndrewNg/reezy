086df561642288f8bf114201174c0b4d5e5c5c43
Andrew Ng <ajng@princeton.edu>
2017-04-19 16:37:40 -0400

    kevins sumamrization code

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
+# -*- coding: utf-8 -*-
+
 from flask import Flask, render_template, request, redirect, make_response
 from flask_sslify import SSLify
 from werkzeug.utils import secure_filename
-import os, json, glob, io, uuid
+import os, json, glob, io, uuid, sys, string, operator
 import pytesseract
 from wand.image import Image
 from PIL import Image as PImage
@@ -9,6 +11,9 @@ from gtts import gTTS
 from tempfile import TemporaryFile
 import boto3
 import pusher
+import nltk
+from collections import Counter
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 app = Flask(__name__)
 app.config.update(
@@ -59,13 +64,15 @@ def status():
 def process():
   # check if the post request has the file part
   if 'file' not in request.files:
-    return json.dumps({'data':'no file dumbass'});
+    return json.dumps({'data':'sorry, no file'});
   # receiving file
   file = request.files['file']
+  length = int(request.form['length'])
+
   # if user does not select file, browser also
   # submit a empty part without filename
   if file.filename == '':
-    return json.dumps({'data':'no file name dumbass'});
+    return json.dumps({'data':'please submit a file with a name'});
   if file and allowed_file(file.filename):
     fname = secure_filename(file.filename)
     fname_without_extension = fname.split('.')[0]
@@ -81,17 +88,21 @@ def process():
       with img.convert('png') as converted:
         pusher_client.trigger(session_id, 'my-event', {'message': 'processing pdf', 'progress': 40})
         for single_img in converted.sequence:
-          img_page = Image(image=img)
+          img_page = Image(image=single_img)
           req_image.append(img_page.make_blob('png'))
-        pusher_client.trigger(session_id, 'my-event', {'message': 'performing OCR', 'progress': 70})
+        pusher_client.trigger(session_id, 'my-event', {'message': 'performing OCR', 'progress': 50})
         for final_img in req_image:
           response_string = response_string + pytesseract.image_to_string(PImage.open(io.BytesIO(final_img)).convert('RGB'))
 
   else:
-    response_string = 'u didnt upload a pdf u liar'
+    response_string = 'please only upload a pdf'
+
+  # summarization
+  pusher_client.trigger(session_id, 'my-event', {'message': 'summarizing', 'progress': 60})
+  response_string = summarize(response_string.replace('\n', ' '), length)
 
   # text to speech
-  pusher_client.trigger(session_id, 'my-event', {'message': 'converting to mp3', 'progress': 90})
+  pusher_client.trigger(session_id, 'my-event', {'message': 'converting to mp3', 'progress': 70})
   tts = gTTS(text=response_string, lang='en')
   f = TemporaryFile()
   tts.write_to_fp(f)
@@ -108,6 +119,70 @@ def process():
   return json.dumps({'data':response_string, 'unique_url':url});
 
 # helper methods
+def summarize(text, n):
+  # process the text
+  lower = text.lower()
+  remover = str.maketrans('','',string.punctuation)
+  words = lower.translate(remover)
+  itertokens = {}
+  itertokens['test'] = words
+
+  # create tfidf vectorizer
+  tfidf = TfidfVectorizer(tokenizer = tokenize, stop_words = nltk.corpus.stopwords.words('english'))
+  weights = tfidf.fit_transform(itertokens.values())
+  names = tfidf.get_feature_names()
+  tfidfs = {}
+  for item in weights.nonzero()[1]:
+    tfidfs[names[item]] = weights[0,item]
+
+  # split text into sentences
+  sentences = nltk.tokenize.sent_tokenize(text)
+  scores = {}
+  for i in range(0,len(sentences)):
+    scores[i] = get_score(sentences[i], tfidfs)
+  sorts = sorted(scores.items(), key=operator.itemgetter(1), reverse=True)
+
+  # select top n sentences (from user input)
+  indices = []
+  for i in range(0,n):
+    indices.append(sorts[i][0]) # to protect from threading issues
+
+  # output corresponding sentences
+  output = ''
+  sort_indices = sorted(indices)
+  for index in sort_indices:
+    output = output + sentences[index] + ' '
+
+  return output
+
+def stem(tokens, stemmer):
+  stems = []
+  for t in tokens:
+    stems.append(stemmer.stem(t))
+  return stems
+
+def tokenize(text):
+  stemmer = nltk.stem.snowball.SnowballStemmer("english") #, ignore_stopwords=True) # uses nltk stopwords
+  tokens = nltk.word_tokenize(text.lower())
+  stemmed = stem(tokens, stemmer)
+  return stemmed
+
+def get_score(sentence, tfidfs):
+  remover = str.maketrans('','',string.punctuation)
+  searchable = tokenize(sentence.translate(remover))
+  filtered = [w for w in searchable if not w in nltk.corpus.stopwords.words('english')]
+  sent_len = len(filtered)
+  if sent_len != 0:
+    score = 0.
+    for word in filtered:
+      if word not in tfidfs:
+        print(word)
+      else:
+        score = score + tfidfs[word]
+    return score/sent_len
+  else:
+    return 0
+
 def allowed_file(filename):
   return '.' in filename and \
     filename.rsplit('.', 1)[1].lower() in set(['pdf'])
